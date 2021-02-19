4204556bf2a5c564c85160aef5ea8fcddacef9f8
Erineo de León <Erineodeleon5@gmail.com>
2021-02-19 13:30:05 -0600

    Hola mundo

---
+++ funciones/HolaMundo.js
@@ -0,0 +1,7 @@
+var nombre
+    /* los var nos sirve para definir una variable */
+var apellido = 'leon '
+    /* aqui le estamos dando de una sola ves un valor a la variable  */
+nombre = 'Erineo'
+    /* le estamos dando un valor a la variable nombre */
+console.log('hola  soy ' + nombre + ' ' + apellido + 'y este es mi primer hola mundo en js')
